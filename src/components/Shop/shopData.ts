f2bf39092bdcb5044b3c30449d1f8fdff7ac70eb
damola <thedamolaoladipo@gmail.com>
2025-11-05 11:31:40 +0100

    feat: added shop data

--- src/components/Shop/shopData.ts
+++ src/components/Shop/shopData.ts
@@ -3,8 +3,8 @@ const shopData: Product[] = [
   {
     title: "Havit HV-G69 USB Gamepad",
     reviews: 15,
-    price: 59.0,
-    discountedPrice: 29.0,
+    price: 55799.0,
+    discountedPrice: 25799.0,
     id: 1,
     imgs: {
       thumbnails: [
@@ -20,8 +20,8 @@ const shopData: Product[] = [
   {
     title: "iPhone 14 Plus , 6/128GB",
     reviews: 5,
-    price: 899.0,
-    discountedPrice: 99.0,
+    price: 89999.0,
+    discountedPrice: 59999.0,
     id: 2,
     imgs: {
       thumbnails: [
@@ -37,8 +37,8 @@ const shopData: Product[] = [
   {
     title: "Apple iMac M1 24-inch 2021",
     reviews: 5,
-    price: 59.0,
-    discountedPrice: 29.0,
+    price: 55799.0,
+    discountedPrice: 25799.0,
     id: 3,
     imgs: {
       thumbnails: [
@@ -54,8 +54,8 @@ const shopData: Product[] = [
   {
     title: "MacBook Air M1 chip, 8/256GB",
     reviews: 6,
-    price: 59.0,
-    discountedPrice: 29.0,
+       price: 55799.0,
+    discountedPrice: 25799.0,
     id: 4,
     imgs: {
       thumbnails: [
@@ -71,8 +71,8 @@ const shopData: Product[] = [
   {
     title: "Apple Watch Ultra",
     reviews: 3,
-    price: 99.0,
-    discountedPrice: 29.0,
+    price: 85799.0,
+    discountedPrice: 55799.0,
     id: 5,
     imgs: {
       thumbnails: [
@@ -88,8 +88,8 @@ const shopData: Product[] = [
   {
     title: "Logitech MX Master 3 Mouse",
     reviews: 15,
-    price: 59.0,
-    discountedPrice: 29.0,
+    price: 45799.0,
+    discountedPrice: 30499.0,
     id: 6,
     imgs: {
       thumbnails: [
@@ -105,8 +105,8 @@ const shopData: Product[] = [
   {
     title: "Apple iPad Air 5th Gen - 64GB",
     reviews: 15,
-    price: 59.0,
-    discountedPrice: 29.0,
+    price: 55799.0,
+    discountedPrice: 25799.0,
     id: 7,
     imgs: {
       thumbnails: [
@@ -122,8 +122,8 @@ const shopData: Product[] = [
   {
     title: "Asus RT Dual Band Router",
     reviews: 15,
-    price: 59.0,
-    discountedPrice: 29.0,
+       price: 55799.0,
+    discountedPrice: 25799.0,
     id: 8,
     imgs: {
       thumbnails: [
